81f68107b8c04beba42aeeb70fcd7ed2844beba1
程泽华 <chengzehua@outlook.com>
2018-03-04 13:52:42 +0800

    Update Kalman Filter

--- Term2/Lab/kalman_filter.py
+++ Term2/Lab/kalman_filter.py
@@ -3,8 +3,8 @@
 
 from math import *
 
+
 class matrix:
-    
     # implements basic operations of a matrix class
 
     def __init__(self, value):
@@ -13,7 +13,7 @@ class matrix:
         self.dimy = len(value[0])
         if value == [[]]:
             self.dimx = 0
-    
+
     def zero(self, dimx, dimy):
         # check if valid dimensions
         if dimx < 1 or dimy < 1:
@@ -22,7 +22,7 @@ class matrix:
             self.dimx = dimx
             self.dimy = dimy
             self.value = [[0 for row in range(dimy)] for col in range(dimx)]
-    
+
     def identity(self, dim):
         # check if valid dimension
         if dim < 1:
@@ -33,12 +33,11 @@ class matrix:
             self.value = [[0 for row in range(dim)] for col in range(dim)]
             for i in range(dim):
                 self.value[i][i] = 1
-    
+
     def show(self):
         for i in range(self.dimx):
-            print self.value[i]
-        print ' '
-    
+            print(self.value[i])
+
     def __add__(self, other):
         # check if correct dimensions
         if self.dimx != other.dimx or self.dimy != other.dimy:
@@ -51,7 +50,7 @@ class matrix:
                 for j in range(self.dimy):
                     res.value[i][j] = self.value[i][j] + other.value[i][j]
             return res
-    
+
     def __sub__(self, other):
         # check if correct dimensions
         if self.dimx != other.dimx or self.dimy != other.dimy:
@@ -64,7 +63,7 @@ class matrix:
                 for j in range(self.dimy):
                     res.value[i][j] = self.value[i][j] - other.value[i][j]
             return res
-    
+
     def __mul__(self, other):
         # check if correct dimensions
         if self.dimy != other.dimx:
@@ -78,7 +77,7 @@ class matrix:
                     for k in range(self.dimy):
                         res.value[i][j] += self.value[i][k] * other.value[k][j]
             return res
-    
+
     def transpose(self):
         # compute transpose
         res = matrix([[]])
@@ -87,17 +86,17 @@ class matrix:
             for j in range(self.dimy):
                 res.value[j][i] = self.value[i][j]
         return res
-    
+
     # Thanks to Ernesto P. Adorio for use of Cholesky and CholeskyInverse functions
-    
+
     def Cholesky(self, ztol=1.0e-5):
         # Computes the upper triangular Cholesky factorization of
         # a positive definite matrix.
         res = matrix([[]])
         res.zero(self.dimx, self.dimx)
-        
+
         for i in range(self.dimx):
-            S = sum([(res.value[k][i])**2 for k in range(i)])
+            S = sum([(res.value[k][i]) ** 2 for k in range(i)])
             d = self.value[i][i] - S
             if abs(d) < ztol:
                 res.value[i][i] = 0.0
@@ -105,40 +104,40 @@ class matrix:
                 if d < 0.0:
                     raise ValueError, "Matrix not positive-definite"
                 res.value[i][i] = sqrt(d)
-            for j in range(i+1, self.dimx):
+            for j in range(i + 1, self.dimx):
                 S = sum([res.value[k][i] * res.value[k][j] for k in range(self.dimx)])
                 if abs(S) < ztol:
                     S = 0.0
-                res.value[i][j] = (self.value[i][j] - S)/res.value[i][i]
+                res.value[i][j] = (self.value[i][j] - S) / res.value[i][i]
         return res
-    
+
     def CholeskyInverse(self):
         # Computes inverse of matrix given its Cholesky upper Triangular
         # decomposition of matrix.
         res = matrix([[]])
         res.zero(self.dimx, self.dimx)
-        
+
         # Backward step for inverse.
         for j in reversed(range(self.dimx)):
             tjj = self.value[j][j]
-            S = sum([self.value[j][k]*res.value[j][k] for k in range(j+1, self.dimx)])
-            res.value[j][j] = 1.0/tjj**2 - S/tjj
+            S = sum([self.value[j][k] * res.value[j][k] for k in range(j + 1, self.dimx)])
+            res.value[j][j] = 1.0 / tjj ** 2 - S / tjj
             for i in reversed(range(j)):
-                res.value[j][i] = res.value[i][j] = -sum([self.value[i][k]*res.value[k][j] for k in range(i+1, self.dimx)])/self.value[i][i]
+                res.value[j][i] = res.value[i][j] = -sum(
+                    [self.value[i][k] * res.value[k][j] for k in range(i + 1, self.dimx)]) / self.value[i][i]
         return res
-    
+
     def inverse(self):
         aux = self.Cholesky()
         res = aux.CholeskyInverse()
         return res
-    
+
     def __repr__(self):
         return repr(self.value)
 
 
 def kalman_filter(x, P):
     for n in range(len(measurements)):
-        
         # measurement update
         Z = matrix([[measurements[n]]])
         y = Z - H * x
@@ -155,18 +154,17 @@ def kalman_filter(x, P):
 
 
 if __name__ == '__main__':
-
     measurements = [1, 2, 3]
 
-    x = matrix([[0.], [0.]]) # initial state (location and velocity)
-    P = matrix([[1000., 0.], [0., 1000.]]) # initial uncertainty
-    u = matrix([[0.], [0.]]) # external motion
-    F = matrix([[1., 1.], [0, 1.]]) # next state function
-    H = matrix([[1., 0.]]) # measurement function
-    R = matrix([[1.]]) # measurement uncertainty
-    I = matrix([[1., 0.], [0., 1.]]) # identity matrix
+    x = matrix([[0.], [0.]])  # initial state (location and velocity)
+    P = matrix([[1000., 0.], [0., 1000.]])  # initial uncertainty
+    u = matrix([[0.], [0.]])  # external motion
+    F = matrix([[1., 1.], [0, 1.]])  # next state function
+    H = matrix([[1., 0.]])  # measurement function
+    R = matrix([[1.]])  # measurement uncertainty
+    I = matrix([[1., 0.], [0., 1.]])  # identity matrix
 
-    print kalman_filter(x, P)
+    print(kalman_filter(x, P))
     # output should be:
     # x: [[3.9996664447958645], [0.9999998335552873]]
     # P: [[2.3318904241194827, 0.9991676099921091], [0.9991676099921067, 0.49950058263974184]]
