00ea716f35d76353006063425fd694c78b28b9d3
程泽华 <chengzehua@outlook.com>
2018-03-04 17:06:37 +0800

    Update Term0 Translate Python to Cpp Project.

---
+++ Term0/P3-Translate-Python-To-C/cmake-build-debug/CMakeFiles/fuckCPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuckCPP.dir/tests.cpp.obj"
+  "fuckCPP.pdb"
+  "fuckCPP.exe"
+  "fuckCPP.exe.manifest"
+  "libfuckCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fuckCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
