b2204070d952766858acec8ce1e58a2321295885
程泽华 <chengzehua@outlook.com>
2018-04-07 23:49:52 +0800

    :sparkles: Add dataset inspect

--- Term1/Object-Detection[Mask RCNN]/coco.py
+++ Term1/Object-Detection[Mask RCNN]/coco.py
@@ -1,7 +1,32 @@
 """
 Mask R-CNN
 Configurations and data loading code for MS COCO.
+
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 coco.py train --dataset=/path/to/coco/ --model=coco
+
+    # Train a new model starting from ImageNet weights
+    python3 coco.py train --dataset=/path/to/coco/ --model=imagenet
+
+    # Continue training a model that you had trained earlier
+    python3 coco.py train --dataset=/path/to/coco/ --model=/path/to/weights.h5
+
+    # Continue training the last model you trained
+    python3 coco.py train --dataset=/path/to/coco/ --model=last
+
+    # Run COCO evaluatoin on the last model you trained
+    python3 coco.py evaluate --dataset=/path/to/coco/ --model=last
 """
+
 import os
 import time
 import numpy as np
